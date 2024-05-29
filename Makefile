4e04fe493de7b1beb7c30697147519d68d728eb6
riskyprsty <66415817+riskyprsty@users.noreply.github.com>
2024-05-29 12:24:45 +0700

    makefile

--- Makefile
+++ Makefile
@@ -4,12 +4,11 @@ PROG1 = mysql_func
 CC = gcc
 CFLAGS = `pkg-config --cflags gtk+-3.0`
 LIBS = `pkg-config --libs gtk+-3.0`
-WALL =`-Wall`
 MYSQL1 =`mysql_config --cflags`
 MYSQL2 = `mysql_config --libs`
 
 ${PROG}: ${PROG}.c
-	${CC} ${WALL} ${CFLAGS} -o ${PROG} ${PROG1}.c ${PROG}.c ${LIBS} ${MYSQL1} ${MYSQL2} 
+	${CC} -Wall ${CFLAGS} -o ${PROG} ${PROG1}.c ${PROG}.c ${LIBS} ${MYSQL1} ${MYSQL2} 
 
 clean:
 	rm ${PROG} ${PROG1}
---
+++ test.c
@@ -0,0 +1,43 @@
+#include <mysql.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    MYSQL *conn;
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+
+    char *server = "localhost";
+    char *user = "root";
+    char *password = "er"; /* set me first */
+    char *database = "myber";
+
+    conn = mysql_init(NULL);
+
+    /* Connect to database */
+    if (!mysql_real_connect(conn, server,
+                            user, password, database, 0, NULL, 0))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    /* send SQL query */
+    if (mysql_query(conn, "show tables"))
+    {
+        fprintf(stderr, "%s\n", mysql_error(conn));
+        exit(1);
+    }
+
+    res = mysql_use_result(conn);
+
+    /* output table name */
+    printf("MySQL Tables in mysql database:\n");
+    while ((row = mysql_fetch_row(res)) != NULL)
+        printf("%s \n", row[0]);
+
+    /* close connection */
+    mysql_free_result(res);
+    mysql_close(conn);
+}
