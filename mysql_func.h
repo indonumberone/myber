df67276c04585d4428b1f77f484a799482fdd87b
riskyprsty <66415817+riskyprsty@users.noreply.github.com>
2024-06-01 15:56:11 +0700

    tes

--- mysql_func.h
+++ mysql_func.h
@@ -1,7 +1,7 @@
 
 int connect_mysql();
 int check_block(char *user);
-int login(char *user, char *pw);
+int login_user(char *user, char *pw);
 int register_user(char *user, char *nama, char *pw);
 int blocked(char *user);
 int freeblock(char *user);
